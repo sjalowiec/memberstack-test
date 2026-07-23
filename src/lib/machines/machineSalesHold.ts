03a6e6fc01c14b52391a9e30e7abb1b31d942863
sjalowiec <sue@knititnow.com>
2026-07-23 06:26:14 -0700

    Temporarily pause machine sales for launch

---
+++ src/lib/machines/machineSalesHold.ts
@@ -0,0 +1,5 @@
+/**
+ * Temporary launch hold while machine shipping/ordering is being finalized.
+ * Set to `false` to restore the full /shop/machines catalog and product pages.
+ */
+export const MACHINE_SALES_HOLD = true;
